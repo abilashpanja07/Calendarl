f0fd52455187010e7410c8108071773f56b0ac6e
abilashpanja07 <60227766+abilashpanja07@users.noreply.github.com>
2020-06-14 12:21:49 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,4 +1,131 @@
-<?php 
-include_once("index.html");
-
- ?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="keywords" content="" />
+<meta name="description" content="" />
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+<title>Calendarl</title>
+<link href="style.css" rel="stylesheet" type="text/css" media="screen" />
+</head>
+<body>
+<div id="header">
+	<div id="logo">
+		<h1 style = "font-size:45px; color:white">Calendarl</h1>
+	</div>
+</div>
+<!-- end #header -->
+<div id="menu">
+	<ul>
+		<li  class="current_page_item">
+		<li><a href="signup.htm">Signup</a></li>
+		<li><a href="login form.htm">Login</a></li>
+	</ul>
+</div>
+<!-- end #menu -->
+<div id="page">
+	<div id="content">
+		<div class="post">
+			<h1 style = "color:white;" class="title">Our Vision</h1>
+			<p class="meta">
+			<div class="entry">
+				<p style = "color:white;">You get the best of both worlds: user friendly booking system provided by Calendarl, and the ease of use and practicality of Google Calendar. It would take your schedule management to a whole new level. People spend hours every day to manage their bookings and schedules, making it one of their top priority in their daily routine.  </p>
+				<p style = "color:white;">Our vision is to provide a feasible way for our users to set up their convinient slots which helps our users to book the available slots as per their needs.</p>
+			<p class="meta">
+			</div>
+		</div>
+		<div class="post">
+			<h2 style = "color:white;" class="title">We grow with you</h2>
+			<p class="meta">
+			<div class="entry">
+				<p style = "color:white;">Big or small businesses... we love serving you all. You can have unlimited services and unlimited clients. We know that even if you have just started with our website, our user friendly interface will lead you to seamless usage of our website. </p>
+				<p style = "color:white;">We also know that as your practice grows, the more advanced your needs become. It is our goal to make your </p>
+				<p class="meta">
+			</div>
+		</div>
+	</div>
+	<!-- end #content -->
+	<div id="sidebar">
+		<ul>
+			<li>
+				<h2 style = "color:white;"> Contact Us</h2>
+				<p>Phone No: 044 43870021 <br> Email: calendarl@gmail.com <br></p>
+			</li>
+			<li id="calendar">
+				<h2 style = "color:white;">Calendar</h2>
+				<div id="calendar_wrap">
+					<table summary="Calendar">
+						<caption>
+						June 2020
+						</caption>
+						<thead>
+							<tr>
+								<th abbr="Monday" scope="col" title="Monday">M</th>
+								<th abbr="Tuesday" scope="col" title="Tuesday">T</th>
+								<th abbr="Wednesday" scope="col" title="Wednesday">W</th>
+								<th abbr="Thursday" scope="col" title="Thursday">T</th>
+								<th abbr="Friday" scope="col" title="Friday">F</th>
+								<th abbr="Saturday" scope="col" title="Saturday">S</th>
+								<th abbr="Sunday" scope="col" title="Sunday">S</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>1</td>
+								<td>2</td>
+								<td>3</td>
+								<td>4</td>
+								<td>5</td>
+								<td>6</td>
+								<td>7</td>
+							</tr>
+							<tr>
+								
+								<td>8</td>
+								<td>9</td>
+								<td>10</td>
+								<td>11</td>
+								<td>12</td>
+								<td>13</td>
+								<td>14</td>
+							</tr>
+							<tr>
+								<td>15</td>
+								<td>16</td>
+								<td>17</td>
+								<td>18</td>
+								<td>19</td>
+								<td>20</td>
+								<td>21</td>
+							</tr>
+							<tr>
+								
+								<td>22</td>
+								<td>23</td>
+								<td>24</td>
+								<td>25</td>
+								<td>26</td>
+								<td>27</td>
+								<td>28</td>
+							</tr>
+							<tr>
+								
+								<td>29</td>
+								<td>30</td>
+							</tr>
+							
+						</tbody>
+					</table>
+				</div>
+			</li>
+		</ul>
+	</div>
+	<!-- end #sidebar -->
+	<div style="clear: both;">&nbsp;</div>
+</div>
+<!-- end #page -->
+<div id="footer">
+	
+</div>
+<!-- end #footer -->
+</body>
+</html>
